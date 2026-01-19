540a95fcaf811b0eef273f67eeeea8a8b17a9309
Manikandan S <ceitmanikandan24@gmail.com>
2026-01-19 21:39:48 +0530

    simple console statements for test

--- hello.js
+++ hello.js
@@ -1,2 +1,14 @@
 console.log("hello");
-console.log("simple test code for node")
+console.log("simple test code for node");
+
+// Global objects 
+console.log(global);
+setTimeout(()=>
+    {
+        console.log("time-out function in node fron Global object");
+    },2000);
+
+    // used to get the current directory name
+    console.log(__dirname);
+    // use to see the fail name 
+    console.log(__filename)
